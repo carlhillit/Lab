c545314ec6f407a03255ae7fe21f8c7853f3f6ea
Carl Hill <carl@carlhill.it>
2022-08-24 21:41:00 +0200

    removed example groups and tested working

--- ActiveDirectory/CreateUsersAndGroups.ps1
+++ ActiveDirectory/CreateUsersAndGroups.ps1
@@ -1,9 +1,11 @@
+# untested
+
 $Domain = 'karl.lab'
 $UsersCsv = Import-Csv -Path .\50CommonNames-EN.csv
 $UsersOU = "OU=Users,OU=HOMELAB,DC=karl,DC=lab"
 $GroupsOU = "OU=Groups,OU=HOMELAB,DC=karl,DC=lab"
 
-$DefaultUserPassword = Read-Host -Prompt 'Enter the default user password' | ConvertTo-SecureString -AsPlainText -Force
+$DefaultUserPassword = Read-Host -AsSecureString -Prompt 'Enter the default user password'
 
 foreach ($user in $UsersCsv) {
     $splat = @{
@@ -21,25 +23,12 @@ foreach ($user in $UsersCsv) {
 }
 
 # create Groups
-$roles = $UsersCsv.Role | Get-Unique
-foreach ($role in $roles){
-    New-ADGroup -Name $role -Path $GroupsOU -GroupCategory Security -GroupScope Universal
+$departments = $UsersCsv.Department | Get-Unique
+foreach ($department in $departments) {
+    New-ADGroup -Name $departments -Path $GroupsOU -GroupCategory Security -GroupScope Universal
 }
 
 # add users to groups
-foreach ($role in $roles) {
-    Get-ADUser -Filter { Description -like $role } -Properties Description | 
-        ForEach-Object { Add-ADGroupMember $role -Members $_.SamAccountName }
-}
-
-# create 5 example groups
-1..5 | ForEach-Object { New-ADGroup -Name "ExampleGroup$_" -GroupCategory Security -GroupScope Global -Path $GroupsOU }
-
-# randomly add users to example groups
-$Users = Get-ADUser -Filter * -SearchBase $UsersOU
-
-foreach ($user in $Users) {
-    $rdm = Get-Random -Minimum 1 -Maximum 5
-    $rdmgrp = "ExampleGroup$rdm"
-    Add-ADGroupMember -Identity $rdmgrp -Members $user
+foreach ($user in $UsersCsv) {
+	Add-ADGroupMember -Identity $user.Department -Members "$($user.GivenName).$($user.SurName)"
 }
