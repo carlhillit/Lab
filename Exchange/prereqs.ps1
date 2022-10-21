72de8587b291ab8d76f63349884512529452a50b
Carl Hill <carl.l.hill@outlook.com>
2022-10-21 20:49:40 +0200

    new

--- Exchange/prereqs.ps1
+++ Exchange/prereqs.ps1
@@ -37,4 +37,5 @@ $RequiredFeatures = @(
 Install-WindowsFeature $RequiredFeatures
 
 
-#$dotnet48url = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe"
+$dotnet48url = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe"
+Start-BitsTransfer $dotnet48url
