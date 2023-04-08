17ab0413327e0026fd1165ebcad2cf3b6859b423
Carl Hill <carl.l.hill@outlook.com>
2023-04-08 22:34:29 +0200

    add comments

--- VMware/updateESXi.sh
+++ VMware/updateESXi.sh
@@ -5,12 +5,17 @@
 # put system in mainenance mode
 esxcli system maintenanceMode set --enable=true
 
+# allow firewall acces
 esxcli network firewall ruleset set --enable=true --ruleset-id=httpClient
 
+# grab latest esxi version
 newversion=$(esxcli software sources profile list --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml | grep standard | tail -1 | awk '{ print $1 }')
 
+# perform update
 esxcli software profile update --depot=https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml --profile=$newversion 
 
+# set firewall back
 esxcli network firewall ruleset set -enable=false --=ruleset-id=httpClient
 
+# reboot
 esxcli system shutdown reboot --reason="updated to $newversion"
